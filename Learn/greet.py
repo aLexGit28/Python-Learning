9ca6c92daf51e401e0f543448f7ff5bbf87312c2
Anurag Sharma <alexandersharma28@gmail.com>
2026-09-01 19:43:18 +0530

    Added greet and square functions with user input and output demonstration

---
+++ Learn/greet.py
@@ -0,0 +1,5 @@
+def greet(name):
+    print(f'Hello {name}! Welcome tp Python!')
+    
+n = input('Enter your name: ')
+greet(n)
---
+++ Learn/square.py
@@ -0,0 +1,5 @@
+def square(x):
+    print(x) #30
+    return x*x # 100
+
+print(square(16))
